c4276824d33f4b9405bedbc3118fd68cb188861e
Nicole Lescano Alomia <nl77656@srcs.loc>
2018-04-27 11:29:12 -0700

    addition

---
+++ 2Dshapes.js
@@ -0,0 +1,31 @@
+var button = document.getElementById('calculateArea');
+
+
+
+
+function Rectangles() {
+    var length = document.getElementById("inputL");
+    var width = document.getElementById("inputW");
+    document.getElementById("squarerectangleoutput").textContent = length * width
+
+}
+
+function Hexagon {
+    var sides = document.getElementById("inputS");
+
+}
+
+
+
+function Triangle() {
+    var base = document.getElementById("inputB");
+    var height = document.getElementById("inputH");
+
+}
+
+function Cicrle() {
+    var radius = document.getElementById("inputR")
+
+
+
+}
